1baf6955ff0f2b3b2edb9c608198f88a94bebf3c
Franklin Tse <FranklinWhale@users.noreply.github.com>
2019-11-27 02:30:18 +0800

    Update pxt

--- monacoworker.js
+++ monacoworker.js
@@ -1,5 +1,1 @@
-// see https://github.com/Microsoft/monaco-editor
-self.MonacoEnvironment = { 
-    baseUrl: "/pxt-microbit-test/" 
-};
-importScripts("/pxt-microbit-test/vs/base/worker/workerMain.js");
+self.MonacoEnvironment={baseUrl:"/pxt-microbit-test/"},importScripts("/pxt-microbit-test/vs/base/worker/workerMain.js");
